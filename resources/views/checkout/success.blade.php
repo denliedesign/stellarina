fb05fffdb6f6b130807abd1a4e72481faddbf0bf
denliedesign <customdenlie@gmail.com>
2024-09-25 13:20:50 -0500

    success with back home button

--- resources/views/checkout/success.blade.php
+++ resources/views/checkout/success.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container my-5">
         <h1>Checkout Successful</h1>
         <p>Your payment has been processed successfully. Thank you for your purchase!</p>
+        <a class="btn btn-pink btn-lg shadow" href="/">Home</a>
     </div>
 @endsection
